0d4dc1aee5e64d19ede20981e1a37e478e42d25b
Vladimir Mano <bujinkank1@gmail.com>
2023-07-18 13:35:48 -0300

    Hello

---
+++ main.py
@@ -0,0 +1,13 @@
+from fastapi import FastAPI
+
+app = FastAPI() # instance
+
+@app.get("/") # the protocol used on this interaction is the standard 'https' - 'http'. get = call or pull from server
+async def root(): # this function will act whenever it cantm and dont depent of a constant server sync.
+    return "Hola FastAPI!"
+
+
+@app.get("/url")
+async def root():
+    return {"url : https:// miwebsite.com/FastAPI!"}
+
