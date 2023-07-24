20401ad5c1e08cba39718c08e58c8ed327570d0e
Vladimir Mano <bujinkank1@gmail.com>
2023-07-24 13:13:08 -0300

    Http status codes added

--- users.py
+++ users.py
@@ -2,7 +2,7 @@
 
 ## Get de Usuarios ##
 
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
 
 app = FastAPI()
@@ -71,23 +71,15 @@ def search_users(id: int):
 
 ## POST ##
 
-@app.post("/user/") # operacion para incertar nuevos datos
+@app.post("/user/",response_model= User, status_code=201) # operacion para incertar nuevos datos, ademas se puede fdefinir
 async def user(user: User):
     if type(search_users(user.id)) == User:
-        return {"Error" : "El usuario ya existe"}
+        raise HTTPException(status_code=204, detail= "El usuario ya existe") #$$ con raise retorna el error correcto por la consola
      
     users_list.append(user)
     return user
 
-
-def search_users(id: int):
-    users = filter(lambda user: user.id == id, users_list)
-
-    try: # comprobacion aqui
-        return list(users)[0]
-    except:
-        return {"error": "No se ha encontrado el usuario"}    
-    
+# tiene que bastar con el codigo de error para informar el usuario de APi que esta pasando.    
 
 ## PUT ##
 
@@ -131,3 +123,27 @@ def search_users(id: int):
         return list(users)[0]
     except:
         return {"error": "No se ha encontrado el usuario"} 
+    
+"""
+## Aprender a utilizar los codigos de status HTTP ##
+
+for more reference use the link below:
+https://developer.mozilla.org/en-US/docs/Web/HTTP/Status
+
+es muy importante documentar los codigos de status de nuestras API para las personas que la consumen.
+
+Eg:
+
+100 cannoy have a body
+200 all ok
+201 after creating something new
+204 not content to return to the client
+300 Redirection
+301 permanent redirection
+307 temp redirection
+304 not modifier
+400 an error
+404 not found
+500 internal server error
+
+"""
