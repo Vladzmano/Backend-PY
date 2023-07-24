fb4b5a88fd5c5da7fec319e1776cb9cfbd99acd0
Vladimir Mano <bujinkank1@gmail.com>
2023-07-24 17:46:54 -0300

    static resources

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from fastapi import FastAPI
 from routers import products
 from routers import users
+from fastapi.staticfiles import StaticFiles
+
 
 app = FastAPI() # instance
 
@@ -8,12 +10,20 @@ app = FastAPI() # instance
 
 app.include_router(products.router)
 app.include_router(users.router)
+app.mount("/static", StaticFiles(directory="static"), name= "static")
+## http://127.0.0.1:8000/static/images/Dark%20laser%20and%20saltin.gif
+
+# Routers
 
 @app.get("/") # the protocol used on this interaction is the standard 'https' - 'http'. get = call or pull from server
 async def root(): # this function will act whenever it cantm and dont depent of a constant server sync.
     return "Hola FastAPI!"
 
+
+
 # Url local : http://102.0.0.1:8000
+
+
 
 @app.get("/url")
 async def url():
