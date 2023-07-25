35b1045b8f5031fce32a009f3f00ea5ff89f8f34
Vladimir Mano <bujinkank1@gmail.com>
2023-07-25 15:17:00 -0300

    oauth2 crypted

--- routers/jwt_auth_users.py
+++ routers/jwt_auth_users.py
@@ -1,10 +1,18 @@
+## OAUTH2 JWT ##
+
 from fastapi import FastAPI, Depends, HTTPException, status
 from pydantic import BaseModel
 from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
+from jose import jwt
+from passlib.context import CryptContext
 
-app = FastAPI() # instance
+ALGORITHM = "HS256"
 
-oauth2 = OAuth2PasswordBearer(tokenUrl="login") # sistema de seguridad buid-in FastaPI
+app = FastAPI()
+
+oauth2 = OAuth2PasswordBearer(tokenUrl="login")
+
+crypt = CryptContext(schemes=["bcrypt"])
 
 class User(BaseModel):
     username: str
@@ -38,26 +46,6 @@ def search_user_db(username: str):
     if username in users_db:
         return UserDB(**users_db[username])
     
-def search_user(username: str):
-    if username in users_db:
-        return User(**users_db[username])
-    
-
-    
-async def current_user(token: str =Depends(oauth2)): # criterio de dependencia, siempre tipar el token
-    user = search_user(token)
-    if not users_db:
-        raise HTTPException(
-            status_code =status.HTTP_401_UNAUTHORIZED, 
-            detail= "Credenciales de autenticacion invalidas", 
-            headers= {"WWW-Authenticate" : "Bearer"})
-    
-    if user.disabled:
-        raise HTTPException(
-            status_code=status.HTTP_400_BAD_REQUEST,
-            detail="Usuario inactivo")
-
-    return user
 
 @app.post("/login")
 async def login(form: OAuth2PasswordRequestForm = Depends()):
@@ -73,11 +61,3 @@ async def login(form: OAuth2PasswordRequestForm = Depends()):
     
 
     return {"access_token": user.username, "token_type": "bearer" }
-
-@app.get("/users/me")
-async def me(user: User = Depends(current_user)):
-    return user
-
-
-## OAUTH2 JWT ##
-
